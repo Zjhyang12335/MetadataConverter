968487dbdfcc441bc87d5662bbb449dc33e23666
labalityowo <56186498+labalityowo@users.noreply.github.com>
2022-07-12 10:43:16 +0700

    feat: initial commit

---
+++ crypto/metadata.h
@@ -0,0 +1,10 @@
+#ifndef METADATA_H
+#define METADATA_H
+
+#include <cstdint>
+#include <cstdlib>
+
+void decrypt_global_metadata(uint8_t *data, size_t size);
+void encrypt_global_metadata(uint8_t *data, size_t size);
+
+#endif //METADATA_H
